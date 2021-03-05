d24873c649efde934297a58ee5b91e837dba402e
alexanderjaramillo <contacto@alexanderjaramillo.com>
2021-03-05 01:15:06 -0500

    loading global state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,21 @@
 import React, { useEffect } from 'react'
-import { Provider, useDispatch } from 'react-redux'
+import { Provider, useDispatch, useSelector } from 'react-redux'
 import { createWrapper } from 'next-redux-wrapper'
 
 import { store } from 'redux/store'
 import { hideGlobalLoader } from 'redux/actions/global'
 
 import 'tailwindcss/tailwind.css'
+import { GlobalState } from 'redux/state/global'
 
 // makeStore function that returns a new store for every request
 const makeStore = () => store
 const Wrapper = createWrapper(makeStore)
 
 function MyApp({ Component, pageProps }) {
+  const globalState = useSelector(
+    (state: { global: GlobalState }) => state.global
+  )
   const dispatch = useDispatch()
 
   useEffect(() => {
@@ -20,7 +24,7 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      {globalState.loading ? 'Please wait...' : <Component {...pageProps} />}
     </Provider>
   )
 }
